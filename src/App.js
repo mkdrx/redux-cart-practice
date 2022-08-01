1ff548719106c71a5338506aa7925b13f52b6ff4
MDX <markusdrx@gmail.com>
2022-08-01 12:37:58 +0200

    added comment

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ let isInitial = true;
 function App() {
   const dispatch = useDispatch();
 
-  // Selectors
+  // Selectors:
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
